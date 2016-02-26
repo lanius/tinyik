3d9e63bc4bddc2ac3ca7be252766241e2a9ba8f8
lanius <lanius@nirvake.org>
2016-02-27 01:41:11 +0900

    Split an optimizer from the IK solver

--- tinyik/core.py
+++ tinyik/core.py
@@ -75,28 +75,43 @@ class FKSolver(object):
         )[:3]
 
 
+class Optimizer(object):
+    """An optimizer based on Newton's method."""
+
+    def __init__(self, f, tol=1.48e-08, maxiter=50):
+        """Generate an optimizer from an objective function."""
+        self.g = autograd.grad(f)
+        self.h = autograd.hessian(f)
+        self.tol = tol
+        self.maxiter = maxiter
+
+    def optimize(self, x0, target):
+        """Calculate an optimum argument of an objective function."""
+        x = x0
+        for _ in range(self.maxiter):
+            delta = np.linalg.solve(self.h(x, target), -self.g(x, target))
+            x = x + delta
+            if np.linalg.norm(delta) < self.tol:
+                break
+        return x
+
+
 class IKSolver(object):
     """An inverse kinematics solver."""
 
-    def __init__(self, fk_solver):
+    def __init__(self, fk_solver, opt_cls=Optimizer, opt_params=None):
         """Generate an IK solver from a FK solver instance."""
         def distance_squared(angles, target):
             x = target - fk_solver.solve(angles)
             return np.sum(np.power(x, 2))
 
-        self._g = autograd.grad(distance_squared)
-        self._h = autograd.hessian(distance_squared)
+        if opt_params is None:
+            opt_params = {}
+        self.optimizer = opt_cls(distance_squared, **opt_params)
 
-    def solve(self, target, angles0, tol=1.48e-08, maxiter=50):
+    def solve(self, target, angles0):
         """Calculate joint angles and returns it."""
-        angles = np.array(angles0)
-        for _ in range(maxiter):
-            delta = np.linalg.solve(
-                self._h(angles, target), -self._g(angles, target))
-            angles = angles + delta
-            if np.linalg.norm(delta) < tol:
-                break
-        return angles
+        return self.optimizer.optimize(np.array(angles0), target)
 
 
 class Link(object):
