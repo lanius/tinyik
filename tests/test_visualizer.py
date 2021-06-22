d9e96e880b4e49d8fa374531168715f1e12ccf8d
lanius <lanius@nirvake.org>
2021-06-22 23:22:59 +0900

    Add test for #18

--- tests/test_visualizer.py
+++ tests/test_visualizer.py
@@ -38,8 +38,16 @@ def large_visualize_with_target():
     tinyik.visualize(arm, target=[400., -300., 0.], radius=15.)
 
 
+def visualize_with_z_axis():
+    arm = tinyik.Actuator(
+        ['z', [0, 0, 180.7], 'y', [-612.7, 0, 0], 'y', [-571.55, 0, 0],
+        'y', [0, -174.15, 0], 'z', [0, 0, -119.85], 'y', [0, -116.55, 0]])
+    tinyik.visualize(arm, radius=10.)
+
+
 if __name__ == '__main__':
     visualize()
     visualize_with_target()
     large_visualize()
     large_visualize_with_target()
+    visualize_with_z_axis()
